7611b9cc6c5d5e8255c02fccdbd043da3f0ed595
unknown <ojaradonatello>
2022-08-02 13:04:35 +0300

    first commit

---
+++ src/components/data/dummydata.js
@@ -0,0 +1,236 @@
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, CloudOutlined, FavoriteBorder, Public, PersonOutlined, AddLocationAltOutlined, PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube } from "@mui/icons-material"
+
+export const navlink = [
+  {
+    url: "/",
+    text: "Home",
+  },
+  {
+    url: "/about",
+    text: "About",
+  },
+  {
+    url: "/services",
+    text: "Services",
+  },
+  {
+    url: "/portfolio",
+    text: "Portfolio",
+  },
+  {
+    url: "/testimonials",
+    text: "Testimonials",
+  },
+  {
+    url: "/blog",
+    text: "Blog",
+  },
+  {
+    url: "/contact",
+    text: "Contact",
+  },
+]
+export const home = [
+  {
+    text: "HELLO I'M",
+    name: "OJARA DONATO",
+    post: "WEB DESIGNER",
+    design: "App Designer",
+    desc: "Experienced Web Developer adept in all stages of advanced web development,Knowledgeable in user interface, testing, and debugging processes. Bringing forth expertise in design, installation, testing and maintenance of web systems.Equipped with a diverse and promising skill-set. Proficient in an assortment of technologies, including HTML, JavaScript, CSS, PHP, REACTJS, WORDPRESS and GIT, MYSQL. Able to effectively self-manage during independent projects, as well as collaborate in a team setting.",
+  },
+]
+export const about = [
+  {
+    desc: "As a tech person, I am passionate and highly driven in the areas of Coding world-class web and mobile products. I have perfected my skills in combining usability with visual design to create perfect user friendly websites. I am a fast learner and I adapt very fast to new and emerging areas in the technological field. I am always up to date and I keep up with new trends in technology.  , I am able to use the most modern, up-to-date technologies to develop software solutions for my clients. Currently I have worked on a number of challenging and up to date technology projects that have required me to develop picture-perfect, clean, and well-tested code. ",
+    desc1: "My three year experience in front end has exposed me to a broad range of projects, from which I picked up new skills and continued to develop my competencies further. On a personal level, I am determined to delve deeper into coding and I continue to look out for opportunities and experts with similar interests to help me further hone my skills. I want to use my knowledge and skills to solve some of the challenges affecting the world today.",
+    cover: "./images/man.png",
+  },
+]
+export const services = [
+  {
+    id: 1,
+    icon: <Settings />,
+    title: "Creative Design",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 2,
+    icon: <CropRotate />,
+    title: "Clean Code",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 3,
+    icon: <ViewInAr />,
+    title: "Responsive Design",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 4,
+    icon: <PieChart />,
+    title: "Material UI	",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 5,
+    icon: <Code />,
+    title: "Material UI Icons",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 6,
+    icon: <BarChart />,
+    title: "Awesome Support",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+]
+export const project = [
+  {
+    id: 1,
+    icon: <CloudOutlined />,
+    num: "89",
+    title: "HAPPY CLIENTS",
+  },
+  {
+    id: 2,
+    icon: <FavoriteBorder />,
+    num: "231",
+    title: "PROJECTS COMPLEATED",
+  },
+  {
+    id: 3,
+    icon: <Public />,
+    num: "108",
+    title: "FILES DOWNLOADED",
+  },
+  {
+    id: 4,
+    icon: <PersonOutlined />,
+    num: "1446",
+    title: "LIENS OF CODE",
+  },
+]
+export const portfolio = [
+  {
+    id: 1,
+    cover: "../images/port/port1.jpg",
+    name: "Brand",
+    category: "marketing",
+    title: "Brex Logo",
+  },
+  {
+    id: 2,
+    cover: "../images/port/port2.jpg",
+    name: "Brand",
+    category: "design",
+    title: "Brex Logo",
+  },
+  {
+    id: 3,
+    cover: "../images/port/port3.jpg",
+    name: "Brand",
+    category: "development",
+    title: "Brex Logo",
+  },
+  {
+    id: 4,
+    cover: "../images/port/port4.jpg",
+    name: "Brand",
+    category: "marketing",
+    title: "Brex Logo",
+  },
+  {
+    id: 5,
+    cover: "../images/port/port5.jpg",
+    name: "Brand",
+    category: "design",
+    title: "Brex Logo",
+  },
+  {
+    id: 6,
+    cover: "../images/port/port6.jpg",
+    name: "Brand",
+    category: "development",
+    title: "Brex Logo",
+  },
+]
+export const testimonials = [
+  {
+    id: 1,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+    image: "./images/testimonials/team1.jpg",
+    name: "Donato Dona",
+    post: "Front End Developer",
+  },
+  {
+    id: 2,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+    image: "./images/testimonials/team-2.png",
+    name: "Alex Ander",
+    post: "Back End Developer",
+  },
+  {
+    id: 3,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+    image: "./images/testimonials/team-3.png",
+    name: "GorkCoder",
+    post: "React Developer",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    title: "Master These Awesome",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+    cover: "./images/blog/b1.png",
+  },
+  {
+    id: 2,
+    title: "Best Design Items to Appeal",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+    cover: "./images/blog/b2.png",
+  },
+  {
+    id: 3,
+    title: "The 20 Best Lightroom Presets",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+    cover: "./images/blog/b3.png",
+  },
+]
+export const contact = [
+  {
+    icon: <AddLocationAltOutlined />,
+    text1: "The Innovation VIllage",
+    text2: "Gulu City,Uganda",
+  },
+  {
+    icon: <PhoneIphone />,
+    text1: "0789329338",
+    text2: "0705055714",
+  },
+  {
+    icon: <EmailOutlined />,
+    text1: "ojaradonato4@gmail.com",
+    text2: "ojaradonato4@gmail.com",
+  },
+]
+export const social = [
+  {
+    icon: <Facebook />,
+  },
+  {
+    icon: <Twitter />,
+  },
+  {
+    icon: <Instagram />,
+  },
+  {
+    icon: <YouTube />,
+  },
+]
